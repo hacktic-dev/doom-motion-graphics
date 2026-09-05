812a653030735e725455f91cf1c788f0e111f015
hacktic <foorganders@outlook.com>
2026-09-05 17:06:02 +0200

    Use custom motion canvas in project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import {defineConfig} from 'vite';
 import motionCanvas from '@motion-canvas/vite-plugin';
-import ffmpeg from '@motion-canvas/ffmpeg';
 
 export default defineConfig({
-  plugins: [
-    motionCanvas(),
-    ffmpeg(),
-  ],
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+  plugins: [motionCanvas()],
 });
